dd6b9e83102ddc9e9740347bf70a5defaa5106a9
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-04-26 01:05:06 +0530

    Delete api

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,17 @@ app.post("/api/products",async(req,res)=>{
 
 })
 
+app.delete("/api/products/:id",async(req,res)=>{
+const {id} = req.params;
+try{
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({success:true,message:"Product deleted"});
+}
+catch(error){
+    console.log("Error in Delete Product: ",error.message);
+    res.status(500).json({success:false,message:`Product not found because: ${error.message}`});
+}
+});
 
 app.listen(5000,()=>{
     connectDB();
