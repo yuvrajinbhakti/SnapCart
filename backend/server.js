a803b86dc1cac3c2d0e8f1a1786800263c29b5c6
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-04-25 09:03:57 +0530

    Mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+dotenv.config();
+
 const app=express();
+
+app.get("/",(req,res)=>{
+res.send("Server is ready");
+});
+
 app.listen(5000,()=>{
+    connectDB();
     console.log("Server is running on port 5000");
 });
