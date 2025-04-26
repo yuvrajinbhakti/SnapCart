bc485b4e71564087d10d9e0aa2360aba3cc11c91
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-04-26 19:50:57 +0530

    minor fix

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -37,13 +37,16 @@ export const createProduct = async(req,res)=>{
 
 export const deleteProduct = async(req,res)=>{
     const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false,message:"Invalid product id"});
+    }
     try{
         await Product.findByIdAndDelete(id);
         res.status(200).json({success:true,message:"Product deleted"});
     }
     catch(error){
         console.log("Error in Delete Product: ",error.message);
-        res.status(500).json({success:false,message:`Product not found because: ${error.message}`});
+        res.status(500).json({success:false,message:`Server error,Product not found because: ${error.message}`});
     }
     };
 
@@ -54,7 +57,7 @@ export const updateProduct = async(req,res)=>{
         return res.status(400).json({success:false,message:"Product not found"});
     }
     if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).json({success:false,message:"Invalid product id"});
+        return res.status(404).json({success:false,message:"Invalid product id"});
     }
     try{
         await Product.findByIdAndUpdate(id,product,{new:true});
--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,13 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app=express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());  //middleware(function which runs before we send response back to client) to parse request body (allows to accpt JSON data in req.body)
 
 app.use("/api/products",productRoutes);     // /api/products will be appended to all api i.e. prefixed with /api/products
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     connectDB();
-    console.log("Server is running on port 5000");
+    console.log("Server is running on http://localhost:"+PORT);
 });
