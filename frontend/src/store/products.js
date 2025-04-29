31b246899cb8518d77bfb9132fbb72180c2ea5a3
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-04-29 11:25:08 +0530

    Update product

--- frontend/src/store/products.js
+++ frontend/src/store/products.js
@@ -34,5 +34,19 @@ export const useProductStore = create((set)=>({
     if(!data.success) return {success:false, message:data.message};
     set((state)=>({products:state.products.filter((product)=>product._id !==pid)}));
     return {success:true, message:"Product deleted successfully"};
+   },
+   updateProduct: async (pid,updatedProduct)=>{
+    const res = await fetch(`/api/products/${pid}`,{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify(updatedProduct),
+    });
+    const data = await res.json();
+    if(!data.success) return {success:false,message:data.message};
+    // update the ui immediately,without need to refresh the page
+    set((state)=>({products:state.products.map((product)=>product._id ===pid?data.data:product)}))
+    return {success:true, message:"Product updated successfully"}
    }
 }));
