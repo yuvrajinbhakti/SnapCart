fe18938f91546101a96cb56d02d05ffb2b16126e
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-04-29 18:05:23 +0530

    made changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from './config/db.js';
 import productRoutes from "./routes/product.route.js";
 dotenv.config();
@@ -7,9 +9,17 @@ dotenv.config();
 const app=express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 app.use(express.json());  //middleware(function which runs before we send response back to client) to parse request body (allows to accpt JSON data in req.body)
 
 app.use("/api/products",productRoutes);     // /api/products will be appended to all api i.e. prefixed with /api/products
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist"))); //making frontend/dist(which we got on running npm run build in frontend) folder static so that it can be served as static files
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT,()=>{
     connectDB();
