a913f1f15c40b25fe0fa3054d61ba83706d6346d
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-05-15 00:42:51 +0530

    Updaing backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // Determine the base URL for API calls
 const apiBaseUrl = import.meta.env.PROD 
-  ? 'https://snap-cart-kap9.vercel.app/api'
+  ? 'https://snap-cart-4gh8.vercel.app/api'
   : '/api';
 
 console.log('API Base URL:', apiBaseUrl);
